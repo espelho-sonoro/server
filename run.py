4ea7d02c26e671b6fd0f8ddf7dd0bfc03b48b170
Mateus Dubiela Oliveira <mateusduboli@gmail.com>
2016-03-24 14:44:31 -0300

    Run flask in debug mode

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ import flask, os
 
 app = flask.Flask(__name__, static_folder='public', static_url_path='')
 port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+app.run(host='0.0.0.0', port=port, debug=True)
