089a5fd32311458b95da2078fe33e1a620f6864b
Mateus Dubiela Oliveira <mateusduboli@gmail.com>
2016-03-23 21:11:36 -0300

    Fixed heroku app initialization

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-import flask
+import flask, os
 
 app = flask.Flask(__name__, static_folder='public', static_url_path='')
-app.run()
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
 
 @app.route('/')
 def root():
