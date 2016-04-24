5ff18ddb129a409bf032885b3431c46c77dfb66f
Mateus Dubiela Oliveira <mateusduboli@gmail.com>
2016-04-24 17:50:46 -0300

    Initialize server using socketio

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def root():
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
-from app import app
+from app import socketio,app
 
 port = int(os.environ.get("PORT", 5000))
 
-app.run(host='0.0.0.0', port=port, debug=True)
+socketio.run(app, host='0.0.0.0', port=port, debug=True)
