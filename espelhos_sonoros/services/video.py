450928566e9fa360d534deb59315221617b00378
Mateus Dubiela Oliveira <mateusduboli@gmail.com>
2017-06-23 21:46:16 -0300

    Reactivate youtube request

--- espelhos_sonoros/services/video.py
+++ espelhos_sonoros/services/video.py
@@ -45,6 +45,6 @@ def video_service(app, socketio, video_dao):
 
     @app.route('/api/videos', methods=['GET'])
     def list_videos_json():
-        return flask.jsonify(VIDEOS)
-        #return flask.jsonify(list(map(lambda v: v._asdict(), video_dao.list())))
+        #return flask.jsonify(VIDEOS)
+        return flask.jsonify(list(map(lambda v: v._asdict(), video_dao.list())))
 
